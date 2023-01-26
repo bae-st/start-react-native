5cb3296732fd673ef1f1a3a4cf1745a7766b6f64
elena <creative.apme@gmail.com>
2023-01-26 23:42:43 +0900

    add custom button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MyButton from './myButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: 'blue'}}>Hi, there</Text>
+      <Text>Hi, there</Text>
       <StatusBar style="auto" />
+      <Button title="Button" onPress={() => alert('click!')} />
+      <MyButton />  
     </View>
   );
 }
---
+++ src/myButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { TouchableOpacity, View, Text } from 'react-native';
+
+const MyButton = () => {
+    return (
+        <TouchableOpacity onPress={() => alert('my button')}
+            // hitSlop={{ bottom: 100, top: 100, left: 100, right: 100}}
+            pressRetentionOffset={{ bottom: 100, top: 10, left: 10, right: 10 }}
+        >
+            <View style={{ backgroundColor: 'red', padding: 10 }}>
+                <Text style={{ fontSize: 20, color: 'white' }}>MyButton</Text>
+            </View>
+        </TouchableOpacity>  
+    );
+};
+
+export default MyButton;
